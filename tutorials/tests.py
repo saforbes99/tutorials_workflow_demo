ca274741d92f74762e8cbda231098b9e8930a839
Scott Forbes <scott@nebulaone.net>
2024-06-09 16:59:15 -0700

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse
+import pytest
+from tutorials.models import Tutorial
+
+
+# Create your tests here.
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+def test_create_tutorial():
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    assert tutorial.title == "Pytest"
